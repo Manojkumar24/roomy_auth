7d8f164c3570a976489f3c35d02002af5315841f
jiteshm17 <33426178+jiteshm17@users.noreply.github.com>
2020-07-24 13:00:03 +0530

    added payments and update room by owner"

--- client/src/components/payments/payments.js
+++ client/src/components/payments/payments.js
@@ -1,46 +1,46 @@
-import React, { Fragment  } from 'react'
-import axios from 'axios';
-import lifecycle from 'react-pure-lifecycle';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
 
-let state = [];
+class Payments extends Component {
+    state = {
+        room: []
+    }
 
-const methods = {
+    constructor(props){
+        super(props);
+        this.displayRazorpay = this.displayRazorpay.bind(this);
+    }
+
     componentDidMount(props) {
         axios.get("/api/rooms/RentHistory").then(res => {
-            console.log(res);
-            state = res.data;
+            this.state.room = res.data;
+            // console.log("res data is",res.data);
+            // state = res.data;
         });
-
-        // console.log(state);
+        // console.log("room is",this.state.room);
     }
-    
-};
 
-function LoadScript(src){
-    return new Promise(resolve => {
-        const script = document.createElement('script')
-        script.src = src
-        //document.body.appendChild(script)
-        script.onload = () => {
-            resolve(true)
-        }
-        script.onerror = () => {
-            resolve(false)
-        }
-        document.body.appendChild(script)
-    
-    })
-}
+     LoadScript (src) {
+        return new Promise(resolve => {
+            const script = document.createElement('script')
+            script.src = src
+            //document.body.appendChild(script)
+            script.onload = () => {
+                resolve(true)
+            }
+            script.onerror = () => {
+                resolve(false)
+            }
+            document.body.appendChild(script)
 
+        })
+    }
 
-function payments(){
-    //const [state, setstate] = useState('Nehul')
-
-    async function displayRazorpay(){
-        const res = await LoadScript("https://checkout.razorpay.com/v1/checkout.js")
-        if(!res){
-             alert('Razorpay SDK failed to load . Are you online..?')
-             return
+    async displayRazorpay() {
+        const res = await this.LoadScript("https://checkout.razorpay.com/v1/checkout.js")
+        if (!res) {
+            alert('Razorpay SDK failed to load . Are you online..?')
+            return
         }
 
         const config = {
@@ -52,13 +52,14 @@ function payments(){
 
         let data = {}
         data = await axios.post('http://localhost:5000/razorpay', config).then(response => {
+            // console.log("returning res data");
             return response.data;
 
         }).catch(error => {
             console.log(error);
         })
-        
-        console.log(data);
+
+        // console.log('data is',data);
         // console.log('front end !!!');
         const options = {
             key: "rzp_test_fvrtPHrPNkX4EA", // Enter the Key ID generated from the Dashboard
@@ -68,8 +69,8 @@ function payments(){
             description: "thank you for ntg please give us more",
             image: "https://i.ibb.co/KqNwVNP/Capture1.jpg",
             order_id: data.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            handler: function (response){
-                
+            handler: function (response) {
+
                 // alert(response.razorpay_payment_id);
                 // alert(response.razorpay_order_id);
                 // alert(response.razorpay_signature)
@@ -79,7 +80,7 @@ function payments(){
                 //name
                 "name": data.name,
                 "email": data.email,
-                "contact": data.contact_number ? (data.contact_number): ("")
+                "contact": data.contact_number ? (data.contact_number) : ("")
             }
             // notes: {
             //     "address": "Razorpay Corporate Office"
@@ -90,36 +91,39 @@ function payments(){
         };
         const paymentObject = new window.Razorpay(options);
         paymentObject.open();
-    }
-    let rentHistory = state ? (
-        state.map(payment =>{ 
-            return(
-            <div>
-                <ul>
-                    <li>{payment.payment_id}</li>
-                    <li>{payment.order_id}</li>
-                    <li>{payment.transaction_date}</li>
-                    <li>{payment.amount}</li>
-                    <li>{payment.transaction_method}</li>
-                </ul>
-            </div>
+}
+
+
+    render() {
+        let rentHistory = this.state.room ? (
+            this.state.room.map(payment => {
+                return (
+                    <div>
+                        <ul>
+                            <li>Payment id: {payment.payment_id}</li>
+                            <li>Order id:  {payment.order_id}</li>
+                            <li>Transaction Date: {payment.transaction_date}</li>
+                            <li>Payment Amount: {payment.amount}</li>
+                            <li>Transaction Method: {payment.transaction_method}</li>
+                        </ul> <br></br>
+                    </div>
+                )
+            })
+        ) : (
+                <div>
+                    <h3>No Transaction History</h3>
+                </div>
             )
-        })
-    ) : (
-            <div>
-                <h3>No Transaction History</h3>
-            </div>
-    )
-    return(
-        <Fragment>
-        <div className="payments">
-            <a onClick={displayRazorpay}> Pay Rent </a>
-            <p> {rentHistory} </p>
-        </div>
-        </Fragment>
-    )
+        return (
+            <Fragment>
+                <div className="payments">
+                    <button onClick={this.displayRazorpay} style={{ margin: "10px" }} className="waves-effect waves-light btn-large" >Pay Rent</button>
+                    <p> {rentHistory} </p>
+                </div>
+            </Fragment>
+        )
 
+    }
+}
 
-};
-
-export default lifecycle(methods)(payments);
+export default Payments;
